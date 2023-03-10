313ac556f0b6a9bf4f1109e97ccbfd1f55de2f23
Patrick Brito <patrickbritokun530@gmail.com>
2023-03-10 08:28:50 -0300

    att Home

--- Pagina_home/script.js
+++ Pagina_home/script.js
@@ -1,1 +1,8 @@
 // Pagina inicial da loja
+function abrirlogin() {
+    document.getElementById("fazerLogin").style.display = "block";
+  }
+  function fecharLogin() {
+    document.getElementById("fazerLogin").style.display = "none";
+  }
+  
